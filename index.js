8771ac9c4614d981889fa8132f49868e65af5651
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-08 20:31:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function takeANumber(katzDeliLine,name){
+   katzDeliLine.push(name)
+  var index = katzDeliLine.length
+  return `Welcome, ${name}. You are number ${index} in line.`
+}
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length>0){
+    var firstperson = katzDeliLine[0]
+    katzDeliLine.shift()
+    return `Currently serving ${firstperson}.`
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
+function currentLine(line){
+  var currentstr = "The line is currently: "
+  if (line.length>0){
+  for (var i = 0, i<line.length,i++){
+      var nameofperson = line[i]
+      if (i != line.length - 1){
+      currentstr.push(`${i}. ${nameofperson}`) += `, `
+       
+        }
+        else{
+          currentstr.push(`${i}. ${nameofperson}`)
+        }
+      }
+       
+    }
+  else{
+    return "The line is currently empty."
+  }
+}  
